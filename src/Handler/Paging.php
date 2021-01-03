39bac89cd5f58def9babac2b81565619f37094d2
thomasop <tdss33@hotmail.com>
2021-01-03 15:37:17 +0100

    Add: paging for readAllUser and readAllProduct

---
+++ src/Handler/Paging.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Handler;
+
+class Paging
+{
+    public $data;
+    public $meta;
+
+    
+    public function __construct($data)
+    {
+        $this->data = $data->getItems();
+        $this->setMeta('limit', $data->getItemNumberPerPage());
+        $this->setMeta('page', $data->getCurrentPageNumber());
+        $this->setMeta('total_items', $data->getTotalItemCount());
+    }
+
+    public function setMeta($name, $value)
+    {
+        $this->meta[$name] = $value;
+    }
+    
+}
